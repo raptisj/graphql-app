4aaa9d130e2ad8085d703d6d14eae59fc971f78b
John Raptis <johnjohn_raptis@yahoo.gr>
2019-06-27 23:52:19 +0300

    trying to update

--- src/Form.js
+++ src/Form.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { query } from './Names';
 // import uuid from 'uuid/v1';
 
 const POST_NAME = gql`
 mutation insert_profile (
-$name: String
-) {
-	insert_profile(
-	objects: [
-	{
-		name: $name
-	}
-	]
+	$name: String
 	) {
-		returning {
-			name
+		insert_profile(
+		objects: [
+		{
+			name: $name
+		}
+		]
+		) {
+			returning {
+				id
+				name
+				editing
+			}
 		}
 	}
-}
-`
+	`
 
 class AddNames extends Component {
 
@@ -37,9 +40,27 @@ class AddNames extends Component {
 			type="text"
 			placeholder="add name"
 			/>
-			<Mutation mutation={POST_NAME} variables={{ name }}>
+			<Mutation mutation={POST_NAME}>
 			{postName => (
-				<button onClick={postName}>
+				<button onClick={() => {
+					postName({
+						variables: {
+							name
+						},
+						update: (store, { data }) => {
+							const stored = store.readQuery({
+								query
+							});
+
+							store.writeQuery({ 
+								query,
+								data: {
+									profile: [ ...stored.profile, ...data.insert_profile.returning]
+								} 
+							});
+						}
+					})
+				}}>
 				Submit
 				</button>
 				)}
--- src/Names.js
+++ src/Names.js
@@ -1,51 +1,143 @@
-import React from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import DeleteProfile from './DeleteProfile';
-// const DELETE_PROFILE = gql`
-// mutation deleteName($itemId: ID) {
-// 	delete_profile(where: { id: { _eq: $itemId } }) {
-// 		affected_rows
-// 	}
-// }
-// `
 
-// const handleDel = (deleteName, id, e) => {
-// 	console.log(id);
-// }
+const DELETE_PROFILE = gql`
+mutation deleteName($id: Int) {
+	delete_profile(where: { id: { _eq: $id } }) {
+		affected_rows
+	}
+}
+`
 
-const Names = () => (
-	<Query
-	query={gql`
-		{
-			profile {
-				id
-				name
-			}
-		}
-		`}
-		>
+const UPDATE_PROFILE = gql`
+mutation updateName($id: Int, $name: String) {
+	update_profile(where: {id: {_eq: $id}},
+	_set: {
+		name: $name
+	}) {
+		affected_rows
+	}
+}
+`
+
+export const query = gql`
+{
+	profile {
+		id
+		name
+		editing
+	}
+}
+`;
+
+const Names = () => {
+	const [edit, setEdit] = useState(false);
+	const [newName, setNewName] = useState('');
+
+	const handleEdit = (index) => {
+		const test = [...newName];
+		test[index].editing = true;
+		// return test;
+		console.log(test);
+	}
+
+useEffect(() => {
+console.log('update update');
+})
+
+	return (
+		<Query query={query}>
 		{({ loading, error, data }) => {
 			if (loading) return <p>Wait for it....</p>
 				if (error) return <p>Something went wrong...</p>
-					return (
-						<div>
-						{data.profile.map((dat) => {
-							return <p key={dat.id}>{dat.name}
-							<DeleteProfile itemId={dat.id} />
-							</p>
-							// <Mutation mutation={DELETE_PROFILE} variable={ dat.id }>
-							 // {deleteName => (
-							// 	<button onClick={deleteName}>
-							// 	(Delete)</button>
-								// <button onClick={handleDel.bind(this, deleteName, dat.id)}>(delete)</button>
-							// 	)}
-							// </Mutation>
-						})}
-						</div>
-						)
+					setNewName(data.profile);
+				return (
+					<div>
+					{data.profile.map((dat, index) => {
+						return (
+							<div key={dat.id}>
+							{dat.editing ? (
+								<Fragment>
+								<form>
+								<input type="text" />
+								</form>
+								<Mutation mutation={UPDATE_PROFILE}>
+								{updateName => (
+									<button 
+									onClick={updateName({
+										variables: {
+											name: newName,
+											id: dat.id
+										},
+										update: (store, { data }) => {
+											const stored = store.readQuery({
+												query
+											});
+
+											store.writeQuery({ 
+												query,
+												data: {
+													profile: console.log(newName)
+												} 
+											});
+										}
+									})}>
+									Update</button>
+									)}
+								</Mutation>
+								</Fragment>
+								) : (
+								<Fragment>
+								<span>{dat.name}</span>
+								<Mutation mutation={DELETE_PROFILE}>
+								{deleteName => (
+									<button
+									onClick={() => {
+										deleteName({
+											variables: {
+												id: dat.id
+											},
+											update: (store, { data }) => {
+												const stored = store.readQuery({
+													query
+												});
+												store.writeQuery({ 
+													query,
+													data: {
+														...stored,
+														profile: stored.profile.filter(
+															p => p.id !== dat.id
+															)
+													} 
+												});
+											}
+										});
+									}}>
+									(Delete)
+									</button>
+									)}
+								</Mutation>
+
+								<button onClick={handleEdit.bind(this, index)}>
+								Edit
+								</button>
+								</Fragment>
+								)}
+
+
+								</div>
+								);
+					})}
+					</div>
+					)
 			}}
 			</Query>
-			)
+			);
+}
 
 export default Names;
+
+
+
+
